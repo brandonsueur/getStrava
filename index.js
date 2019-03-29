4343d012d5ac81ec822317c2dd66ae702d34f36d
brandonsueur <gynidark@gmail.com>
2019-03-29 09:43:20 +0100

    feat: add lib strava

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const config = require('./config')
+
+const strava = new require("strava")({
+  client_id: config.client_id, 
+  client_secret: config.client_secret,
+  redirect_uri: config.redirect_uri,
+  access_token: config.access_token
+})
+
+console.log(strava)
+
+strava.athlete.get(function(err, res) {
+  console.log(res)
+})
